bf9423f31c394e14db4f4309082ea6e79506a460
xinggang.li <xinggang.li@horizon.cc>
2024-10-14 15:52:09 +0800

    可以显示图片了。注意设置了环境变量。

--- ImageRegion/ImageRegion.cpp
+++ ImageRegion/ImageRegion.cpp
@@ -2,11 +2,21 @@
 //
 
 #include "ImageRegion.h"
-
+#include <opencv2/opencv.hpp>
+#include <fmt/format.h>
 using namespace std;
 
 int main()
 {
 	cout << "Hello CMake." << endl;
+	auto path = "D:/repos/ImageRegion/h01.exr";
+	cv::Mat mat = cv::imread(path, cv::ImreadModes::IMREAD_UNCHANGED);
+	if (mat.cols <= 0 || mat.rows <= 0)
+	{
+		fmt::println("error, width = {}, height = {}", mat.cols, mat.rows);
+		return -1;
+	}
+	cv::imshow("h01", mat);
+	cv::waitKey(0);
 	return 0;
 }
