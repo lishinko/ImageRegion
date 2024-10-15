39679b114190a77468de028929ffecd4767f48f2
xinggang.li <xinggang.li@horizon.cc>
2024-10-15 11:31:27 +0800

    矩形测试成功，不过确实中间有缝隙

--- ImageRegion/src/ImageRegion.cpp
+++ ImageRegion/src/ImageRegion.cpp
@@ -9,35 +9,43 @@ using namespace std;
 
 int main()
 {
-	auto path = "D:/repos/ImageRegion/h01.exr";
-	cv::Mat mat = cv::imread(path, cv::ImreadModes::IMREAD_UNCHANGED);
-	cv::Mat bgra[4];
-	cv::split(mat, bgra);
-	if (mat.cols <= 0 || mat.rows <= 0)
-	{
-		fmt::println("error, width = {}, height = {}", mat.cols, mat.rows);
-		return -1;
-	}
-	cv::Mat red(bgra[2]);
-	auto t = red.type();
-	cv::Mat gradient(red.rows, red.cols,t);
-	cv::Laplacian(red, gradient, -1);
+    auto path = "D:/repos/ImageRegion/h01.exr";
+    cv::Mat mat = cv::imread(path, cv::ImreadModes::IMREAD_UNCHANGED);
+    cv::Mat bgra[4];
+    cv::split(mat, bgra);
+    if (mat.cols <= 0 || mat.rows <= 0)
+    {
+        fmt::println("error, width = {}, height = {}", mat.cols, mat.rows);
+        return -1;
+    }
+    cv::Mat red(bgra[2]);
+    auto t = red.type();
+    cv::Mat gradient(red.rows, red.cols, t);
+    cv::Laplacian(red, gradient, -1);
 
-	RectGeneration g(red, 0, 0, 0.1, 6);
-	std::vector<Rect> result;
-	cv::Rect r;
-	r.x = 20;
-	r.y = 40;
-	r.width = 100;
-	r.height = 200;
-	cv::rectangle(gradient, r, cv::Scalar(0.4), 2, cv::LineTypes::FILLED);
-	//cv::rectangle(gradient, r, cv::Scalar(0.4));
-	result.reserve(10000);
-	auto ret = g.Generate(result);
-	fmt::println("-------------------------------------ret = {}, retNum = {} quad = {}", result.size(), RectGeneration::RetNum(), RectGeneration::QuardGenNum());
 
-	//cv::imshow("h01", red);
-	cv::imshow("h01_gradiant", gradient);
-	cv::waitKey(0);
-	return 0;
+    RectGeneration g(red, 0, 0, 0.1, 6);
+    std::vector<Rect> result;
+    //cv::rectangle(gradient, r, cv::Scalar(0.4));
+    result.reserve(10000);
+    auto ret = g.Generate(result);
+    fmt::println("-------------------------------------ret = {}, retNum = {} quad = {}", result.size(), RectGeneration::RetNum(), RectGeneration::QuardGenNum());
+
+    cv::Mat debug(red.rows, red.cols, t);
+    for (auto rr : result)
+    {
+        cv::Rect r;
+        r.x = rr.leftBottom.x;
+        r.y = rr.leftBottom.y;
+        r.width = rr.rightBottom.x - rr.leftBottom.x;
+        r.height = rr.leftTop.y - rr.leftBottom.y;
+        //cv::rectangle(debug, r, cv::Scalar(0.4), 2, cv::LineTypes::FILLED);//thickness = 1的情况下，filled会报错
+        cv::rectangle(debug, r, cv::Scalar(0.4));//thickness = 1的情况下，filled会报错
+    }
+
+    //cv::imshow("h01", red);
+    cv::imshow("h01_gradiant", gradient);
+    cv::imshow("h01_debug", debug);
+    cv::waitKey(0);
+    return 0;
 }
